e777106e3fe782172d077b05d893e2625fa5ae22
ravi8545 <139250843+ravi8545@users.noreply.github.com>
2025-12-30 12:17:18 +0530

    learning childNodes in DOM

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
 let div = document.querySelector("div");
-let h1 = document.querySelector("h1");
 
-let body = document.querySelector("body");
-body.addEventListener("keydown", (event)=>{
-  console.log(event.key);
-  h1.innerText=event.key;
-})
+console.log(div.childNodes);
